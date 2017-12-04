ca8269bcb118d62cecf4f65b4868632d96453c00
Conrad Ohlsson <conrad.ohlsson@starrepublic.com>
2017-12-04 09:49:16 +0100

    Add answer to Day4 Step2

--- Day4/index.js
+++ Day4/index.js
@@ -3,27 +3,62 @@ var input = fs.readFileSync("./Day4/input.txt", "utf8");
 
 var input_rows = input.split("\n");
 
-var correct_phrases = 0;
+function Step1(input) {
+  var correct_phrases = 0;
 
-for (var i = 0; i < input_rows.length; i++) {
-  if (input_rows[i] === "") {
-    continue;
-  }
-  var check_array = [];
-  var cols = input_rows[i].split(" ");
-  var valid = true;
+  for (var i = 0; i < input.length; i++) {
+    if (input[i] === "") {
+      continue;
+    }
+    var check_array = [];
+    var cols = input[i].split(" ");
+    var valid = true;
 
-  for (var j = 0; j < cols.length; j++) {
-    if (check_array.indexOf(cols[j]) === -1) {
-      check_array.push(cols[j]);
-    } else {
-      valid = false;
+    for (var j = 0; j < cols.length; j++) {
+      if (check_array.indexOf(cols[j]) === -1) {
+        check_array.push(cols[j]);
+      } else {
+        valid = false;
+      }
+    }
+
+    if (valid) {
+      correct_phrases++;
     }
   }
-
-  if (valid) {
-    correct_phrases++;
-  }
+  return correct_phrases;
 }
 
-console.log(correct_phrases); //325
+function Step2(input) {
+  var correct_phrases = 0;
+
+  for (var i = 0; i < input.length; i++) {
+    if (input[i] === "") {
+      continue;
+    }
+    var check_array = [];
+    var cols = input[i].split(" ");
+    var valid = true;
+
+    for (var j = 0; j < cols.length; j++) {
+      var sortedWord = cols[j]
+        .split("")
+        .sort()
+        .join("");
+
+      if (check_array.indexOf(sortedWord) === -1) {
+        check_array.push(sortedWord);
+      } else {
+        valid = false;
+      }
+    }
+
+    if (valid) {
+      correct_phrases++;
+    }
+  }
+  return correct_phrases;
+}
+
+console.log("Step 1: ", Step1(input_rows)); //325
+console.log("Step 2: ", Step2(input_rows)); //119
